d3c077d9fc68859763928eeb4acceb7670a1a803
tolga tosun <tlgtsn08@gmail.com>
2024-12-17 17:37:12 +0300

    update nav_links modal

--- internal/navLinks/model.go
+++ internal/navLinks/model.go
@@ -4,7 +4,10 @@ import "time"
 
 // Menu represents a menu item
 type NavLink struct {
-	ID        int       `db:"id" json:"id"`
-	Name      string    `db:"name" json:"name"`
-	CreatedAt time.Time `db:"created_at" json:"created_at"`
+	ID          int       `db:"id" json:"id"`
+	Name        string    `db:"name" json:"name"`
+	Label       string    `db:"label" json:"label"`
+	Destination string    `db:"destination" json:"destination"`
+	Href        string    `db:"href" json:"href"`
+	CreatedAt   time.Time `db:"created_at" json:"created_at"`
 }
