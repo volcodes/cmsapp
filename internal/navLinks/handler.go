2f6eb7c6d451997fd47c5af6e506068e21ff497b
tolga tosun <tlgtsn08@gmail.com>
2024-12-17 17:37:18 +0300

    Updated the successful response

--- internal/navLinks/handler.go
+++ internal/navLinks/handler.go
@@ -2,7 +2,6 @@ package navLinks
 
 import (
 	"cms-project/pkg/response"
-	"encoding/json"
 	"net/http"
 )
 
@@ -19,7 +18,7 @@ func GetNavLinksHandler(w http.ResponseWriter, r *http.Request) {
 		response.JSON(w, http.StatusInternalServerError, false, "Failed to fetch menus", nil)
 		return
 	}
-	json.NewEncoder(w).Encode(menus)
+	response.JSON(w, http.StatusOK, true, "Users retrieved successfully", menus)
 }
 
 // CreateMenuHandler handles creating a new menu
